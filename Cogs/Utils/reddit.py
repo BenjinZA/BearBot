9e11f4281e6f7414dd819a6c7a7dc138dbc10c11
BenjinZA <noxza56@gmail.com>
2024-02-02 20:42:06 +0100

    Replace praw with asyncpraw

--- Cogs/Utils/reddit.py
+++ Cogs/Utils/reddit.py
@@ -1,21 +1,24 @@
-import praw
+import asyncpraw
 import json
 
 with open('bot_info.json', 'r') as file:
     bot_info = json.load(file)
 
-reddit = praw.Reddit(client_id=bot_info['reddit_id'],
-                     client_secret=bot_info['reddit_secret'],
-                     user_agent=bot_info['reddit_agent'])
+reddit = asyncpraw.Reddit(client_id=bot_info['reddit_id'],
+                          client_secret=bot_info['reddit_secret'],
+                          user_agent=bot_info['reddit_agent'])
 
 
 async def newUserPost(sr, redditName):
-    u = reddit.redditor(redditName)
+    u = await reddit.redditor(redditName)
 
-    for submission in u.submissions.new(limit=10):
+    async for submission in u.submissions.new(limit=10):
         if submission.subreddit == sr:
             return [submission.url, submission.selftext]
 
+
 async def hotPost(sr):
-    submission = next(x for x in reddit.subreddit(sr).hot() if not x.stickied)
-    return [submission.url, submission.selftext, submission.title]
+    subreddit = await reddit.subreddit(sr)
+    async for submission in subreddit.hot(limit=10):
+        if not submission.stickied:
+            return [submission.url, submission.selftext, submission.title]
