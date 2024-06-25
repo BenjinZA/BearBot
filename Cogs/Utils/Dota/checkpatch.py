356c870227d3aff644a724419580a1b2e15bd779
BenjinZA <noxza56@gmail.com>
2024-06-25 14:04:47 +0200

    Check if url returns text before parsing

--- Cogs/Utils/Dota/checkpatch.py
+++ Cogs/Utils/Dota/checkpatch.py
@@ -5,7 +5,10 @@ import aiohttp
 async def get_patches():
     session = aiohttp.ClientSession()
     patches = await session.get('https://www.dota2.com/datafeed/patchnoteslist')
-    data = json.loads(await patches.text())
+    if patches.text():
+        data = json.loads(await patches.text())
+    else:
+        data = None
     await session.close()
 
     return data
