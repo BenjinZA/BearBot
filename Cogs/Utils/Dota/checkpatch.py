68d9ebc3c09a2d8e45c0ab566bb5259a08534367
BenjinZA <noxza56@gmail.com>
2024-07-01 17:38:21 +0200

    Fix not using await

--- Cogs/Utils/Dota/checkpatch.py
+++ Cogs/Utils/Dota/checkpatch.py
@@ -4,9 +4,10 @@ import aiohttp
 
 async def get_patches():
     session = aiohttp.ClientSession()
-    patches = await session.get('https://www.dota2.com/datafeed/patchnoteslist')
-    if patches.text():
-        data = json.loads(await patches.text())
+    url_response = await session.get('https://www.dota2.com/datafeed/patchnoteslist')
+    patches = await url_response.text()
+    if patches:
+        data = json.loads(patches)
     else:
         data = None
     await session.close()
