214804c3c3bb571656c6f5234fb160207ee51776
BenjinZA <noxza56@gmail.com>
2024-09-24 18:33:56 +0200

    Update saved volume from VolumeButtons

--- Cogs/music.py
+++ Cogs/music.py
@@ -41,67 +41,78 @@ def stop_lavalink(lavalink):
 
 class VolumeButtons(discord.ui.View):
 
-    def __init__(self, player):
+    def __init__(self, player, state):
         super().__init__(timeout=None)
         self.player = player
+        self.state = state
 
     @discord.ui.button(label='10', style=discord.ButtonStyle.grey)
     async def volume_10(self, interaction: discord.Interaction, button: discord.ui.Button):
         await self.player.set_volume(10)
+        self.state.saved_volume = 10
         await interaction.message.delete()
 
     @discord.ui.button(label='20', style=discord.ButtonStyle.grey)
     async def volume_20(self, interaction: discord.Interaction, button: discord.ui.Button):
         await self.player.set_volume(20)
+        self.state.saved_volume = 20
         await interaction.message.delete()
 
     @discord.ui.button(label='30', style=discord.ButtonStyle.grey)
     async def volume_30(self, interaction: discord.Interaction, button: discord.ui.Button):
         await self.player.set_volume(30)
+        self.state.saved_volume = 30
         await interaction.message.delete()
 
     @discord.ui.button(label='40', style=discord.ButtonStyle.grey)
     async def volume_40(self, interaction: discord.Interaction, button: discord.ui.Button):
         await self.player.set_volume(40)
+        self.state.saved_volume = 40
         await interaction.message.delete()
 
     @discord.ui.button(label='50', style=discord.ButtonStyle.grey)
     async def volume_50(self, interaction: discord.Interaction, button: discord.ui.Button):
         await self.player.set_volume(50)
+        self.state.saved_volume = 50
         await interaction.message.delete()
 
     @discord.ui.button(label='60', style=discord.ButtonStyle.grey)
     async def volume_60(self, interaction: discord.Interaction, button: discord.ui.Button):
         await self.player.set_volume(60)
+        self.state.saved_volume = 60
         await interaction.message.delete()
 
     @discord.ui.button(label='70', style=discord.ButtonStyle.grey)
     async def volume_70(self, interaction: discord.Interaction, button: discord.ui.Button):
         await self.player.set_volume(70)
+        self.state.saved_volume = 70
         await interaction.message.delete()
 
     @discord.ui.button(label='80', style=discord.ButtonStyle.grey)
     async def volume_80(self, interaction: discord.Interaction, button: discord.ui.Button):
         await self.player.set_volume(80)
+        self.state.saved_volume = 80
         await interaction.message.delete()
 
     @discord.ui.button(label='90', style=discord.ButtonStyle.grey)
     async def volume_90(self, interaction: discord.Interaction, button: discord.ui.Button):
         await self.player.set_volume(90)
+        self.state.saved_volume = 90
         await interaction.message.delete()
 
     @discord.ui.button(label='100', style=discord.ButtonStyle.grey)
     async def volume_100(self, interaction: discord.Interaction, button: discord.ui.Button):
         await self.player.set_volume(100)
+        self.state.saved_volume = 100
         await interaction.message.delete()
 
 
 class MusicButtons(discord.ui.View):
 
-    def __init__(self, player, music):
+    def __init__(self, player, state):
         super().__init__(timeout=None)
         self.player = player
-        self.music = music
+        self.state = state
 
     @discord.ui.button(label='Pause', emoji='⏸️', style=discord.ButtonStyle.grey)
     async def play_pause(self, interaction: discord.Interaction, button: discord.ui.Button):
@@ -125,12 +136,12 @@ class MusicButtons(discord.ui.View):
 
     @discord.ui.button(label='Set volume', emoji='🔊', style=discord.ButtonStyle.green)
     async def volume_button(self, interaction: discord.Interaction, button: discord.ui.Button):
-        await interaction.channel.send('Pick volume', view=VolumeButtons(self.player))
+        await interaction.channel.send('Pick volume', view=VolumeButtons(self.player, self.state))
         await interaction.response.edit_message(view=self)
 
     @discord.ui.button(label='Disconnect', emoji='⏹️', style=discord.ButtonStyle.red)
     async def disconnect_button(self, interaction: discord.Interaction, button: discord.ui.Button):
-        await self.music.player_disconnect(interaction.guild)
+        await self.state.music.player_disconnect(interaction.guild)
 
 
 class VoiceState:
@@ -149,7 +160,7 @@ class VoiceState:
         embed_music_msg = discord.Embed(title='BearBot Music Player', description=f'Now playing: {song.embed_title}')
         embed_music_msg.set_image(url=song.embed_image)
         if self.music_msg is None:
-            self.music_msg = await self.music_channel.send(embed=embed_music_msg, view=MusicButtons(player, self.music))
+            self.music_msg = await self.music_channel.send(embed=embed_music_msg, view=MusicButtons(player, self))
         else:
             self.music_msg = await self.music_msg.edit(embed=embed_music_msg)
 
